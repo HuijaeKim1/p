8fd330dab8a6185592a78f9a25d17a0f2e30f573
HuijaeKim1 <huijaekim05@gmail.com>
2024-02-23 11:16:08 -0600

    Initial Commit

---
+++ p/pApp.swift
@@ -0,0 +1,17 @@
+//
+//  pApp.swift
+//  p
+//
+//  Created by 64014206 on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
